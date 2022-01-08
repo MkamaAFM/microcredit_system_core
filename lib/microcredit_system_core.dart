a5f38ab078c9fef2efe9562b4147bf3a598704cf
Mkama Anthony Makoko <afmkama@gmail.com>
2022-01-08 10:25:58 +0300

    Added go_router

--- lib/microcredit_system_core.dart
+++ lib/microcredit_system_core.dart
@@ -5,6 +5,7 @@ export 'package:firebase_auth/firebase_auth.dart';
 export 'package:cloud_firestore/cloud_firestore.dart';
 export 'package:firebase_storage/firebase_storage.dart';
 export 'package:flutter_bloc/flutter_bloc.dart';
+export 'package:go_router/go_router.dart';
 //creative_packages
 export 'package:creative_flutter/creative_flutter.dart';
 
